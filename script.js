92886547e0b49bd26318e36a388d749dab468da5
Karleen Heong <17855516+karleenheong@users.noreply.github.com>
2023-10-26 00:00:38 +1100

    Make number buttons functional
    Make number buttons show up as numbers on the calculator display.

--- script.js
+++ script.js
@@ -2,6 +2,79 @@ let num1 = 0;
 let num2 = 0;
 let operator;
 
+const display = document.querySelector(".display");
+//initial display
+let displayValue = "0";
+
+function updateText(newInput){
+  if(displayValue === "0"){
+    displayValue = newInput;
+  } else if(newInput === "clear"){
+    displayValue = "0";
+  } else {
+    displayValue += newInput;
+  }
+  display.textContent = displayValue;
+}
+
+
+//get references to number buttons
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+
+zero.addEventListener("click", () => {
+  updateText("0");
+})
+one.addEventListener("click", () => {
+  updateText("1");
+})
+two.addEventListener("click", () => {
+  updateText("2");
+})
+three.addEventListener("click", () => {
+  updateText("3");
+})
+four.addEventListener("click", () => {
+  updateText("4");
+})
+five.addEventListener("click", () => {
+  updateText("5");
+})
+six.addEventListener("click", () => {
+  updateText("6");
+})
+seven.addEventListener("click", () => {
+  updateText("7");
+})
+eight.addEventListener("click", () => {
+  updateText("8");
+})
+nine.addEventListener("click", () => {
+  updateText("9");
+})
+
+
+//get references to operators
+const division = document.querySelector("#divide");
+const times= document.querySelector("#multiply");
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const equals = document.querySelector("#equals");
+
+//clear function
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {
+ updateText("clear");
+});
+
 
 function add(a, b){
   return a + b;
@@ -22,16 +95,16 @@ function divide(a, b){
 function operate(operator, num1, num2){
   switch(operator) {
     case "+":
-      console.log(add(num1, num2));
+      display.textContent = add(num1, num2);
       break;
     case "-":
-      console.log(subtract(num1, num2));
+      display.textContent = subtract(num1, num2);
       break;
     case "*":
-      console.log(multiply(num1, num2));
+      display.textContent = multiply(num1, num2);
       break;
     case "/":
-      console.log(divide(num1, num2));
+      display.textContent = divide(num1, num2);
       break;
     default:
       console.log("Something's wrong");
